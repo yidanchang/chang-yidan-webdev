2028761a6b4203957d731d9d7a4d7006644b6abc
yidanchang <changyidan1113@gmail.com>
2017-05-11 23:58:29 -0400

    Update app.js

--- test/app.js
+++ test/app.js
@@ -8,8 +8,8 @@ module.exports = function(app)
     if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
         var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
         var password = process.env.MLAB_PASSWORD_WEBDEV;
-        connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+        connectionString = 'mongodb://' + yidanchang + ':' + Grace13940356125;
+        connectionString += 'ds031601.mlab.com:31601/heroku_lk22crx9'; // user yours
     }
 
     var mongoose = require("mongoose");
