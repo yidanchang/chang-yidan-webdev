6e9a64cbaf86ac57427064b272bcba679aa0c584
yidanchang <changyidan1113@gmail.com>
2017-06-13 11:17:08 -0400

    assignment5.5
    For Heroku Application Error

--- assignment/app.js
+++ assignment/app.js
@@ -1,6 +1,19 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/webdev_summer1_2017');
+
+var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds031601.mlab.com:31601/heroku_lk22crx9'; // user yours
+}
+
+var mongoose = require("mongoose");
+mongoose.connect(connectionString);
 mongoose.Promise = require('q').Promise;
+
+// mongoose.connect('mongodb://localhost/webdev_summer1_2017');
+// mongoose.Promise = require('q').Promise;
 
 require("./models/model.server");
 
